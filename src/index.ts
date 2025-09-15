95362be0e12b2dcf0c36dee1490dea4e53cb67dd
girishkhanna1105 <girish1105khanna@gmail.com>
2025-09-15 20:29:52 +0530

    ml get data routes completed

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import signupRoutes from "./Routes/signup.routes.ts"
 import authRoutes from "./Routes/auth.routes.ts"
 import jobOperation from "./Routes/job.routes.ts"
 import oauth from "./Routes/oauth.routes.ts"
+import mlRoute from "./Routes/ml.routes.ts"
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use("/api/v1/register",signupRoutes);
 app.use("/api/v1/auth",authRoutes);
 app.use("api/v1/joboperation",jobOperation)
 app.use("api/v1/oauth",oauth)
+app.use("api/v1/ml",mlRoute)
 
 const port:string=process.env.PORT || "3000";
 
